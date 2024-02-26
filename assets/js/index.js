a6acc863e32878b997a8cf05b5f57220565f5014
emaicorp <emaicorp@gmail.com>
2024-02-26 13:40:35 +0100

    made more changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -42,3 +42,36 @@ window.addEventListener("scroll" ,()=>{
     var nav = this.document.querySelector("nav");
     nav.classList.toggle("sticky", window.scrollY>0)
 })
+// (function(w) {
+//     //private variable
+//     var loaded = false;
+//     w.onload = function() {
+//         loaded = true;
+//     };
+
+//     w.checkLoaded = function() {
+//         alert(loaded);
+//     };
+// })(window);
+
+window.addEventListener("scroll", reveal);   
+function reveal(){
+    let reveals = document.querySelectorAll('.reveal')
+    for(let i = 0; i<reveals.length; i++){
+         let windowHeight = window.innerHeight;
+         let revealTop = reveals[i].getBoundingClientRect().top;
+         let revealPoint = 50;
+         
+
+         if(revealTop < windowHeight - revealPoint  ){
+            reveals[i].classList.add('active');
+            $(document).ready(function () {
+
+            $('#report').load(location.href + " #report");
+        });
+
+         }else{
+            reveals[i].classList.remove('active');
+         }
+    }
+}
